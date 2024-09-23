a4b9b4591734b62bd5515422a6ba2f958ac567b0
Killoconq <fjc1997v@gmail.com>
2024-09-22 21:24:56 -0600

    change little problems

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://killoconq.github.io',
+  base: 'killoconq.github.io',
   integrations: [tailwind()]
 });
