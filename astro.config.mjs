28447724ad063b16830acd02ef942dd4b9ddca31
Killoconq <fjc1997v@gmail.com>
2024-11-10 14:25:55 -0600

    add netlify again

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://killoconq.github.io",
-  base: "killoconq",
+  output: "server",
+  adapter: netlify(),
 });
+
