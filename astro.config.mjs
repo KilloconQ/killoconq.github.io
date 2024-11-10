fc1cc5817bfd99f08e807c549dd677cb01a5a61b
Killoconq <fjc1997v@gmail.com>
2024-11-10 14:09:21 -0600

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://killoconq.github.io",
-  base: "https://github.com/killoconq/killoconq.github.io",
+  base: "killoconq",
 });
