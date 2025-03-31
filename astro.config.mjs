91b86f1788188296225d9e25a2ad1bdf34f4e025
KilloconQ <fjc1997v@gmail.com>
2025-03-30 20:33:38 -0600

    imp: update tailwindcs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
+import tailwindcss from "@tailwindcss/vite";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   output: "server",
   adapter: netlify(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
-
