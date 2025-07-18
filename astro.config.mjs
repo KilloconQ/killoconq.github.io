2937b87c535d2496ee541f5e11984d426e902192
fcorralescarfaxmx <fcorrales@carfaxmx.com.mx>
2025-07-18 11:30:37 -0600

    change  astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://killoconq.github.io',
+  base: 'killoconq.github.io',
   output: 'server',
   vite: {
     plugins: [tailwindcss()],
